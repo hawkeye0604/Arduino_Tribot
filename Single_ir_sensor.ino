38af85f365f870a6de355f3accf2b38e03b2131b
hawkeye0604 <38864697+hawkeye0604@users.noreply.github.com>
2018-06-24 15:20:15 +0530

    Add files via upload
    Program to differentiate between black and white line/area using IR sensor module and serial print.

---
+++ Single_ir_sensor.ino
@@ -0,0 +1,22 @@
+#define out_ir 12 //output pin of ir sensor module
+#define vcc_ir 8 //vcc pin of ir sensor module
+
+void setup() {
+  pinMode(12, INPUT);
+  pinMode(8, OUTPUT);
+  digitalWrite(out_ir, HIGH); 
+  Serial.begin(9600);
+}
+
+void loop() {
+  int sensor_one = 0; //variable which will store value of our sensor reading
+  digitalWrite(vcc_ir, HIGH);
+  sensor_one = digitalRead(out_ir);
+  if(sensor_one == 0){      //sensor reading 0 signifies dark/black line
+    Serial.println("Black");
+  }
+  else{
+    Serial.println("White");
+  }
+
+}
